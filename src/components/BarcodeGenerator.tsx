80fc018a8f31eb0cccabbc8f24c9108872cf9d93
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-10-20 12:08:00 +0600

    update

--- src/components/BarcodeGenerator.tsx
+++ src/components/BarcodeGenerator.tsx
@@ -3,7 +3,7 @@ import Barcode from "react-barcode";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function BarcodeGenerator({ barcode }: { barcode: any }) {
   return (
-    <div className="w-[38mm] h-[25mm] border border-black flex flex-col justify-center items-center overflow-hidden p-[2mm] box-border">
+    <div className="w-[38mm] h-[25mm]  flex flex-col justify-center items-center overflow-hidden p-[2mm] box-border">
       <div className="text-[8px] font-medium text-center leading-tight w-full text-ellipsis whitespace-nowrap">
         {barcode.name}
       </div>
